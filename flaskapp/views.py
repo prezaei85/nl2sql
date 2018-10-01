b6e9ef96aca60d0520b57afa6c788a74854d1727
Ubuntu <ubuntu@ip-172-31-35-117.us-west-2.compute.internal>
2018-10-01 01:48:35 +0000

    adding the base flaskapp files

---
+++ flaskapp/views.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from flask import request
+from flaskapp import app
+# from sqlalchemy import create_engine
+# from sqlalchemy_utils import database_exists, create_database
+# import pandas as pd
+# import psycopg2
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('master.html')
+
+
+@app.route('/go')
+def go():
+    query = request.args.get('query', '')
+    return render_template(
+        'go.html',
+        query=query,
+    )
---
+++ run.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python
+from flaskapp import app
+app.run(host='0.0.0.0', debug=True)
